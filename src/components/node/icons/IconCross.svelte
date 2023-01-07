82c70edfd16f3f244f6caf5953b48f44377fd206
mori <chohyuk0725@gmail.com>
2023-01-07 20:37:07 +0900

    save

--- src/components/node/icons/IconCross.svelte
+++ src/components/node/icons/IconCross.svelte
@@ -7,10 +7,12 @@ export let color = "#5f6b7c";
   id="Layer_1"
   xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink"
+  width="20px"
+  height="20px"
   x="0px"
   y="0px"
   viewBox="0 0 20 20"
-  enable-background="new 0 0 20 20"
+  enable-background="0 0 20 20"
   xml:space="preserve">
   <g id="cross_mark_6_">
     <g>
